8dd96f099115d2cbce52bafeda53b0faa846cce6
MakalisterAndrade <makalister.andrade@outlook.com>
2024-03-21 16:51:55 -0400

    atualização para deploy

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -5,6 +5,10 @@ def main():
     st.title('Dashboard EMD')
 
     # Aqui você pode adicionar mais opções de navegação ou filtros utilizando st.sidebar
+    st.markdown('Para acessar meu currículo em PDF, [clique aqui](https://www.canva.com/design/DAF8I-_rVMk/WttRnU2PHyU76y-fGcNy5w/edit?utm_content=DAF8I-_rVMk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton).', unsafe_allow_html=True)
+    
+    # Adicionando um link para o blog pessoal
+    st.markdown('Para visitar meu blog pessoal, [clique aqui](https://makalister.netlify.app/).', unsafe_allow_html=True)
     
     # Display dos gráficos
     display_graphs_A()
